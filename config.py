9cbca5f6e9821b55c0c8c809106ba543b3fcdce9
DTCMusic <89629350+DTCMusic@users.noreply.github.com>
2023-02-26 04:59:24 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", "19485442"))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "")
-    CHANNEL = os.environ.get("CHANNEL", "")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", ""))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6034110041:AAFuMhPb-JCiNcgVPxiugE9V6sO-QAbl0xw")
+    API_ID = int(os.environ.get("API_ID", "12878302"))
+    API_HASH = os.environ.get("API_HASH", "1ce756e879790d465304f48c36294883")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "RiyadAndMe")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DTGSongBot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "RiyaddBlogg")
+    CHANNEL = os.environ.get("CHANNEL", "RiyaddBlogg")
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001726242069"))
