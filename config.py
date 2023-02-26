ab03849a36a1cf6caff3bd8d5dc724e24946f1fa
DTCMusic <89629350+DTCMusic@users.noreply.github.com>
2023-02-26 05:19:57 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6034110041:AAFuMhPb-JCiNcgVPxiugE9V6sO-QAbl0xw")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5809046574:AAFJ2sKtZ33gB_qYEO8loCWprz1dbldG9R8")
     API_ID = int(os.environ.get("API_ID", "12878302"))
     API_HASH = os.environ.get("API_HASH", "1ce756e879790d465304f48c36294883")
     BOT_OWNER = os.environ.get("BOT_OWNER", "RiyadAndMe")
